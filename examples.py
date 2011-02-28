79d3b1ec30812121899b61243eaf7802a6fa1440
Divye Kapoor <divyekapoor@gmail.com>
2011-03-01 00:09:26 +0530

    Added human players and a small example to test out 4 human players. Note: not working as of now

--- examples.py
+++ examples.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python
 
 from card import Card
+from game import CourtPiece, HumanPlayer
+import sys
+
 
 def print_deck():
-	print "A deck of cards: "
-	for suit in Card.suits:
-		print suit + ": ",
-		for value in range(1,13):
-			print unicode(Card(value, suit)),
-		print
-		
+    print "A deck of cards: "
+    for suit in Card.suits:
+        print suit + ": ",
+        for value in range(1,13):
+            print unicode(Card(value, suit)),
+        print
+
+def play_one_round():
+    # Create a game of Court Piece with 4 players
+    # Note: The input-output file handles are currently stdin and stdout
+    #   They can be easily changed to be socket handles
+    court_piece = CourtPiece([
+        HumanPlayer('Bharat', sys.stdin, sys.stdout), 
+        HumanPlayer('Swagat', sys.stdin, sys.stdout),
+        HumanPlayer('Rohit', sys.stdin, sys.stdout),
+        HumanPlayer('Divye', sys.stdin, sys.stdout),
+        ])
+        
+    court_piece.play()
+    
+            
 if __name__ == "__main__":
-	print_deck()
-	
+    print_deck()
+    play_one_round()
+    
 
--- game.py
+++ game.py
@@ -1,46 +1,73 @@
 #!/usr/bin/env python
 
-class Game(object):
-	
-	def __init__(self):
-		pass
-		
-	def play(self):
-		pass
-		
+class Game(object):    
+    def __init__(self):
+        pass
+        
+    def play(self):
+        pass
+    
+    
+
+class GameState(object):
+    """Store the game state visible publicly. Eg. timers, points etc."""
+    
+    def __init__(self):
+        pass
+        
+    def __repr__(self):
+        return "GameState()"
+        
 
 class Player(object):
-	
-	def __init__(self, name, cards):
-		self.name = name
-		self.cards = cards
-	
-	def __unicode__(self):
-		return self.name
-		
-	def __str__(self):
-		return self.name
-		
-	def __repr__(self):
-		return 'Player(' + repr(self.name) + ', ' + repr(self.cards) + ')'
-	
-	@property
-	def name(self):
-		return self._name
-		
-	@name.setter
-	def name(self, value):
-		if isinstance(value, basestring):
-			self._name = value
-		else:
-			raise ValueError("%r is not a suitable ")
-			
-	@name.deleter
-	def name(self):
-		del self._name
-		
+    def __init__(self, name):
+        self.name = name
+    
+    def __unicode__(self):
+        return self.name
+        
+    def __str__(self):
+        return self.name
+        
+    def __repr__(self):
+        return 'Player(' + repr(self.name) + ')'
+        
+    def play(self, game_state):
+        return None
+    
+
+class HumanPlayer(Player):
+    
+    def __init__(self, name, input_stream, output_stream, prompts=True):
+        Player.__init__(self, name)
+        self.input = input_stream
+        self.output = output_stream
+        self.prompts = prompts
+        
+        
+    def __repr__(self):
+        return 'HumanPlayer(' + repr(self.name) + ')'
+        
+    def play(self, state):
+        if self.prompts:
+            self.output.write("Game State: %s\n%s> " % (state, self))
+            self.output.flush()
+            
+        for line in self.input: # TODO: Fix needed. Lines are not read in one by one...
+            if line != "":
+                value, suit = line.split(' ', 2)
+                return Card(value, suit)
+            else:
+                if self.prompts:
+                    self.output.write("%s> " % self)
+        
+    
 
 class CourtPiece(object):
-	
-	def __init__(self):
-	    pass
+    def __init__(self, players):
+        self.players = players
+        self.game_state = GameState()
+        
+    def play(self):
+        for player in self.players:
+            print "Player (%s) played %s" % (player, player.play(self.game_state))
